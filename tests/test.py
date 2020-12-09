d0fdaed140bfdf74b706223a2d25c57c06a199f0
Yedam <66651590+yefovar@users.noreply.github.com>
2020-12-08 20:14:22 -0600

    Prueba metodo potencia y deflacion #6
    Prueba para git hub action

--- tests/test.py
+++ tests/test.py
@@ -11,10 +11,13 @@ from utils import deflacion
 df = pd.read_csv('data_t_final.csv')
 df = df.dropna(subset=['time','orig_time','first_time']).copy()
 
-# Lectura de base de datos para PCA
-#defaultrates_states1 = df.groupby(['time','state_orig_time'])['default_time'].mean().unstack(level=1).add_prefix('defaultrate_').fillna(0).reset_index(drop=False)
-#scaler = StandardScaler()
-#defaultrates_states = scaler.fit_transform(defaultrates_states1)
+# Transformacion de base de datos para Metodo de potencia y deflacion
+defaultrates_states1 = df.groupby(['time','state_orig_time'])['default_time'].mean().unstack(level=1).add_prefix('defaultrate_').fillna(0).reset_index(drop=False)
+scaler = StandardScaler()
+defaultrates_states = scaler.fit_transform(defaultrates_states1)
+cov = np.cov(defaultrates_states.T)
+
+### Regresion Logistica ###
 
 # Regresion Logistica con metodos de optimizacion
 fitted_values, beta = estim_prob(df,['time','orig_time','first_time','default_time'])
@@ -23,15 +26,25 @@ fitted_values, beta = estim_prob(df,['time','orig_time','first_time','default_ti
 model_lr = smf.glm('default_time ~ time + orig_time + first_time', 
 family = sm.families.Binomial(), data = df).fit()
 
+# Evaluacion de la precision con metodo de optimizacion y con paqueteria de python
+
 print(beta == approx(np.array(model_lr.params), abs=1e-5, rel=1e-5))
 
+### Metodo de potencia y deflacion ###
 
+# PCA con metodos de optimizacion
+
+eigenvalor,eigenvector = potencia(cov,MAX=1000)
+eigenvalor2,eigenvector2 = deflacion(cov,MAX=1000)
+cp1 = eigenvector@defaultrates_states.T
+cp2 = eigenvector2@defaultrates_states.T
 
 # PCA con paqueteria de python
-#pca=PCA()
-#pca.fit(defaultrates_states)
-#z = pca.transform(defaultrates_states)
+pca=PCA()
+pca.fit(defaultrates_states)
+z = pca.transform(defaultrates_states)
 
+# Evaluacion de la precision con metodo de optimizacion y con paqueteria de python
 
-#print(z[:,0]== approx(np.array(potencia(defaultrates_states, sim=False, MAX=100)))
-#print(z[:,1]== approx(np.array(second_potencia(defaultrates_states, MAX=100)))
+print(cp1 == approx(z[:,0], abs=1e-1, rel=1e-1))
+print(-cp2 == approx(z[:,1], abs=1e-1, rel=1e-1))
